5cf7259aea3220177e79ff5ed35b5308b48af749
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-10-21 21:56:32 +0300

    update generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -5,7 +5,7 @@ import { writeContacts } from '../../src/utils/writeContacts.js';
 const generateContacts = async (number) => {
     const contacts = await readContacts();
     const newContacts = Array(number).fill(0).map(createFakeContact);
-    const result = [...contacts, newContacts];
+    const result = [...contacts, ...newContacts];
     await writeContacts(result);
 };
 
